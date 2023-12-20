cedc24f52aee67284e5da0df16c1f5009bd2dae0
Marco Moscatelli <marcomoscatelli.dev@gmail.com>
2023-12-20 05:03:59 +0100

    navbar separate component

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,10 @@
+<nav class="flex justify-center py-4 mt-4 mb-8 mx-24 rounded-2xl shadow-lg">
+    <div class="max-w-7xl mx-16 w-full flex justify-between">
+        <h2 class="text-primary-gradient font-bold text-xl">WPTAT</h2>
+        <ul class="flex gap-12">
+            <li class="hover:font-medium">Politics</li>
+            <li class="hover:font-medium">Economics</li>
+            <li class="hover:font-medium">Environment</li>
+        </ul>
+    </div>
+</nav>

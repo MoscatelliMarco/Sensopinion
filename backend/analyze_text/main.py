b681329cd74a830cc00a867287723a553c1a02e8
Marco Moscatelli <marcomoscatelli.dev@gmail.com>
2024-01-04 04:02:18 +0100

    fix: schedule function now working

--- backend/analyze_text/main.py
+++ backend/analyze_text/main.py
@@ -1,15 +1,8 @@
-import schedule
-import time
 from functions.scheduler import scheduler_function
-
-# Schedule the function to run every 4 hours
-schedule.every(4).hours.do(scheduler_function)
+import time
 
 if __name__ == '__main__':
-    # Run the code instantly
-    scheduler_function()
-
     # Run the code every 4 hours
     while True:
-        schedule.run_pending()
-        time.sleep(60)
+        scheduler_function()
+        time.sleep(60 * 60 * 4) # Sleep 4 hours

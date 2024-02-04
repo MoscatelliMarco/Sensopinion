9096e8fdce2f85a8201f1aac01592647d0129d5c
MoscatelliMarco <marcomoscatelli.dev@gmail.com>
2024-02-04 11:53:03 +0100

    feat: added cryptocurrencies to urls_to_scrape

--- backend/variables.py
+++ backend/variables.py
@@ -11,6 +11,7 @@ urls_to_scrape = [
     "https://news.google.com/u/1/topics/CAAqIggKIhxDQkFTRHdvSkwyMHZNR2RtY0hNekVnSmxiaWdBUAE?hl=en-US&gl=US&ceid=US%3Aen",
     "https://news.google.com/u/1/topics/CAAqIggKIhxDQkFTRHdvSkwyMHZNREp3ZVRBNUVnSmxiaWdBUAE?hl=en-US&gl=US&ceid=US%3Aen",
 
+    "https://news.google.com/topics/CAAqJAgKIh5DQkFTRUFvS0wyMHZNSFp3YWpSZlloSUNaVzRvQUFQAQ?hl=en-US&gl=US&ceid=US%3Aen",
     "https://news.google.com/topics/CAAqIggKIhxDQkFTRHdvSkwyMHZNRGxqTjNjd0VnSmxiaWdBUAE?hl=en-US&gl=US&ceid=US%3Aen",
     "https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNREpmTjNRU0FtVnVLQUFQAQ/sections/CAQiQ0NCQVNMQW9JTDIwdk1ESmZOM1FTQW1WdUlnOElCQm9MQ2drdmJTOHdNVE41TjNrcUN4SUpMMjB2TURFemVUZDVLQUEqJQgAKiEICiIbQ0JBU0Rnb0lMMjB2TURKZk4zUVNBbVZ1S0FBUAFQAQ?hl=en-US&gl=US&ceid=US%3Aen",
     "https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNREpmTjNRU0FtVnVLQUFQAQ/sections/CAQiV0NCQVNPd29JTDIwdk1ESmZOM1FTQW1WdUlnOElCQm9MQ2drdmJTOHdPWGswY0cwcUdnb1lDaFJOUVZKTFJWUlRYMU5GUTFSSlQwNWZUa0ZOUlNBQktBQSolCAAqIQgKIhtDQkFTRGdvSUwyMHZNREpmTjNRU0FtVnVLQUFQAVAB?hl=en-US&gl=US&ceid=US%3Aen"

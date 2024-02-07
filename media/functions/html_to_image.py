3874eae7ba3ae1b849d42ba58f44e6c4faa515ab
MoscatelliMarco <marcomoscatelli.dev@gmail.com>
2024-02-07 21:13:18 +0100

    feat: file saving working, still some bugs to fix

--- media/functions/html_to_image.py
+++ media/functions/html_to_image.py
@@ -1,31 +1,42 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
+import asyncio
+from pyppeteer import launch
+
+async def save_html_as_png(html_content, output_png):
+    browser = await launch(headless=True)
+    page = await browser.newPage()
+
+    # Set content of the page to the provided HTML string
+    await page.setContent(html_content)
+
+    # Set viewport size to the full page size
+    body_width = await page.evaluate('document.body.scrollWidth')
+    body_height = await page.evaluate('document.body.scrollHeight')
+    await page.setViewport({'width': body_width, 'height': body_height})
+
+    # Save screenshot as PNG
+    await page.screenshot({'path': output_png})
+
+    # Close browser
+    await browser.close()
 
 def html_to_image(folder, replacements, output_file = "./posts/demo.png"):
     # Read HTML and CSS content
-    with open(f"./{folder}/template.html", 'r') as file:
+    with open(f"./templates/{folder}/template.html", 'r') as file:
         html_content = file.read()
-    with open(f"./{folder}/style.css", 'r') as file:
+    with open(f"./templates/{folder}/style.css", 'r') as file:
         css_content = file.read()
+    with open(f"./templates/app.css", 'r') as file:
+        tailwind_css_content = file.read()
 
     # Replace placeholders in HTML and CSS
     for key, value in replacements.items():
-        html_content = html_content.replace(f'{{{{ {key} }}}}', value)
+        html_content = html_content.replace("{{" + key + "}}", value)
     
     # Full HTML content including CSS
-    full_html = html_content.replace('<link rel="stylesheet" href="example.css">', f'<style>{css_content}</style>')
-    
-    # Configure Selenium with headless Chrome
-    chrome_options = Options()
-    # chrome_options.add_argument("--headless") TODO put headless back after testing
-    chrome_options.add_argument("--window-size=2000x2000")
-    driver = webdriver.Chrome(options=chrome_options)  # Ensure you have chromedriver installed and in PATH
-    
-    # Set content and render
-    driver.get("data:text/html;charset=utf-8," + full_html)
-    time.sleep(2)  # Adjust as necessary
+    full_html = html_content.replace('<link rel="stylesheet" href="./style.css">', f'<style>{css_content}</style>')
+    full_html = full_html.replace('<link rel="stylesheet" href="../app.css">', f'<style>{tailwind_css_content}</style>')
 
-    # Save screenshot
-    driver.save_screenshot(output_file)
-    driver.quit()
+    print(full_html)
+    
+    # Save file
+    asyncio.get_event_loop().run_until_complete(save_html_as_png(full_html, output_file))
--- media/main.py
+++ media/main.py
@@ -1,4 +1,11 @@
 from functions.html_to_image import html_to_image
 
 if __name__ == "__main__":
-    html_to_image("./templates/most_x_news", {"title": "demo title", "body": "demo body"})
+    html_to_image("most_x_news", {
+        "factor": "negative", 
+        "category": "politics",
+        "news_title": "Biden is killing civiliants and his enjoying it",
+        "image_first_url": "https://asianews.network/wp-content/uploads/2024/02/2532551.jpg"
+        })
+
+    print('post')

0fcc0673c8de8955d8e8431aef27f6f2c92e2c85
MoscatelliMarco <marcomoscatelli.dev@gmail.com>
2024-02-25 22:16:01 +0100

    chores: changed spacing about me

--- frontend/src/routes/info/about-us/+page.svelte
+++ frontend/src/routes/info/about-us/+page.svelte
@@ -3,23 +3,23 @@
     <meta name='description' content="The innovative news analyzer that brings a new perspective to how you understand the world. We specialize in providing insightful analyses of the latest news.">
 </svelte:head>
 
-<div class="flex flex-col word-break gap-6 mt-3 md:mt-4">
+<div class="flex flex-col word-break gap-4.5 mt-3 md:mt-4">
     <div class="flex flex-col">
         <h3 class="text-3xl lg:text-4xl font-medium">About us</h3>
         <h5 class="text-sm font-light text-grey-2">Sensopinion: Your Window to the World's Sentiments</h5>
     </div>
-    <div class="flex flex-col gap-4 lg:gap-6">
+    <div class="flex flex-col gap-6 lg:gap-7">
         <p class="text-sm text-grey-2">
             Welcome to Sensopinion, the innovative news analyzer that brings a new perspective to how you understand the world. We specialize in providing insightful analyses of the latest news, focusing on crucial domains such as the economy, politics, and the environment. Our mission is to offer you a deeper understanding of the global narrative through the lens of emotions and sentiments.
         </p>
 
-        <div class="flex flex-col gap-1">
+        <div class="flex flex-col gap-0.5 lg:gap-1">
             <h6 class="font-semibold text-xl text-grey-1">How We Work</h6>
             <p class="text-sm text-grey-2">
                 Every four hours, Sensopinion harnesses the power of technology to scan through the plethora of articles on Google News. We meticulously analyze news content, extracting key sentiments and emotions that underpin the global conversation. This continuous process ensures that you get timely and relevant insights into the prevailing mood and tone of news across the world.
             </p>
         </div>
-        <div class="flex flex-col gap-1">
+        <div class="flex flex-col gap-0.5 lg:gap-1">
             <span class="font-semibold text-xl text-grey-1">What We Offer</span>
             <p class="text-sm text-grey-2">
                 Comprehensive Coverage: From financial markets to political landscapes, and environmental concerns, we ensure a broad spectrum of news is analyzed.
@@ -30,7 +30,7 @@
             </p>
         </div>
 
-        <div class="flex flex-col gap-1">
+        <div class="flex flex-col gap-0.5 lg:gap-1">
             <span class="font-semibold text-xl text-grey-1">A Note on Accuracy</span>
             <p class="text-sm text-grey-2">
                 We strive for precision and relevance in our analyses. However, it's important to note that Sensopinion relies on artificial intelligence models, which, like any technology, have their limitations. Our results may occasionally be subject to inaccuracies and can potentially 'hallucinate' in categorization, emotion classification, and sentiment classification. We continuously work on improving our models for better accuracy and reliability.

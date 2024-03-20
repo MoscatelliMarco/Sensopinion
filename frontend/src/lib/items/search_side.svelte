b667a079091e285d28eb9db2878a1f8702209a23
MoscatelliMarco <marcomoscatelli.dev@gmail.com>
2024-03-20 21:29:48 +0100

    feat: auto focus on search input when opening it

--- frontend/src/lib/items/search_side.svelte
+++ frontend/src/lib/items/search_side.svelte
@@ -35,10 +35,16 @@
     $: if (search_value !== undefined) {
         updateParams(search_value);
     }
+
+    // Focus on search input when opens
+    import { onMount } from "svelte";
+    onMount(() => {
+        document.querySelector("#search-input").focus();
+    })
 </script>
 
 <div class="lg:absolute lg:top-0 lg:left-0 flex flex-col gap-8 pt-1 pb-3 pr-4 pl-1 w-full lg:w-56 h-full bg-white">
     <div class="bg-transparent border border-grey-2 rounded-md p-0.5">
-        <input bind:value={search_value} maxlength="50" name="url" type="text" placeholder="Search content" class="text-xs md:text-sm px-1.5 py-1 w-full focus:border-none focus:outline-none" />
+        <input id="search-input" bind:value={search_value} maxlength="50" type="text" placeholder="Search content" class="text-xs md:text-sm px-1.5 py-1 w-full focus:border-none focus:outline-none" />
     </div>
 </div>
